729f4ba6a39c582a3263eb97424a8a49407c1b23
yori1995 <ruddufdl789@naver.com>
2023-06-27 16:23:49 +0900

    20230627

--- 02-Ansible/ch04/add_ssh_auth.sh
+++ 02-Ansible/ch04/add_ssh_auth.sh
@@ -4,3 +4,6 @@
 sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.11
 sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.12
 sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.13
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.14
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.15
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.16
